f63783186698a42088de76ecce57e02e204ea178
Axmadjon Isaqov <67532499+axiydev@users.noreply.github.com>
2022-09-05 12:04:55 +0500

    staggered animations

--- lib/pages/sign_in/sign_in_page.dart
+++ lib/pages/sign_in/sign_in_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_splash/pages/sign_up/sign_up_page.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class SignInPage extends StatefulWidget {
@@ -20,64 +21,105 @@ class _SignInPageState extends State<SignInPage> {
       // ),
       body: SizedBox(
         width: double.infinity,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            SizedBox(
-              width: double.infinity,
-              child: Text(
-                'Instagram',
-                textAlign: TextAlign.center,
-                style: GoogleFonts.lobsterTwo(
-                    fontSize: 30, fontWeight: FontWeight.bold),
+        child: AnimationLimiter(
+          child: ListView(
+            shrinkWrap: true,
+            padding:
+                EdgeInsets.only(top: MediaQuery.of(context).size.width * 0.7),
+            children: [
+              getAnimationScale(
+                position: 0,
+                child: SizedBox(
+                  width: double.infinity,
+                  child: Text(
+                    'Instagram',
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.lobsterTwo(
+                        fontSize: 30, fontWeight: FontWeight.bold),
+                  ),
+                ),
               ),
-            ),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-              child: TextField(
-                decoration: InputDecoration(
-                    isDense: true,
-                    hintText: 'Email',
-                    border: OutlineInputBorder()),
+              getAnimationScale(
+                position: 1,
+                child: const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        isDense: true,
+                        hintText: 'Email',
+                        border: OutlineInputBorder()),
+                  ),
+                ),
               ),
-            ),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-              child: TextField(
-                decoration: InputDecoration(
-                    isDense: true,
-                    hintText: 'Password',
-                    border: OutlineInputBorder()),
+              getAnimationScale(
+                position: 2,
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        isDense: true,
+                        filled: true,
+                        fillColor: Colors.grey[300],
+                        hintText: 'Password',
+                        border: const OutlineInputBorder()),
+                  ),
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-              child: SizedBox(
-                width: double.infinity,
-                child: CupertinoButton.filled(
-                    borderRadius: BorderRadius.circular(3),
-                    child: const Text('login'),
-                    onPressed: () {}),
+              getAnimationScale(
+                position: 3,
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: CupertinoButton.filled(
+                        borderRadius: BorderRadius.circular(3),
+                        child: const Text('login'),
+                        onPressed: () {}),
+                  ),
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  const Text('Don\'t have an account?'),
-                  TextButton(
-                      onPressed: () {
-                        Navigator.of(context).pushNamed(SignUpPage.route);
-                      },
-                      child: const Text('Sign up'))
-                ],
+              getAnimationScale(
+                position: 4,
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      const Text('Don\'t have an account?'),
+                      TextButton(
+                          onPressed: () {
+                            Navigator.of(context).pushNamed(SignUpPage.route);
+                          },
+                          child: const Text('Sign up'))
+                    ],
+                  ),
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  Widget getAnimation({required int position, required Widget child}) {
+    return AnimationConfiguration.staggeredList(
+        position: position,
+        duration: const Duration(milliseconds: 450),
+        child: SlideAnimation(verticalOffset: 100, child: child));
+  }
+
+  Widget getAnimationScale({required int position, required Widget child}) {
+    return AnimationConfiguration.staggeredList(
+        position: position,
+        duration: const Duration(milliseconds: 450),
+        child: ScaleAnimation(
+          curve: Curves.easeIn,
+          child: child,
+        ));
   }
 }
--- lib/pages/sign_up/sign_up_page.dart
+++ lib/pages/sign_up/sign_up_page.dart
@@ -10,10 +10,38 @@ class SignUpPage extends StatefulWidget {
   State<SignUpPage> createState() => _SignUpPageState();
 }
 
-class _SignUpPageState extends State<SignUpPage> {
+class _SignUpPageState extends State<SignUpPage>
+    with SingleTickerProviderStateMixin {
   final focusNode1 = FocusNode();
   final focusNode2 = FocusNode();
   final controller = TextEditingController();
+
+  late AnimationController? _animationController;
+  late Tween<double> _tween;
+  late Animation<double> _animation;
+  @override
+  void initState() {
+    _animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 1));
+    _tween = Tween<double>(begin: .0, end: 1);
+    _animation = _tween.animate(
+        CurvedAnimation(parent: _animationController!, curve: Curves.easeIn));
+    _animationController!.forward();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _animationController!.dispose();
+    _animation.removeListener(() {});
+    super.dispose();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,92 +49,98 @@ class _SignUpPageState extends State<SignUpPage> {
       //   title: const Text('sign in'),
       // ),
       body: SizedBox(
-        width: double.infinity,
+        width: MediaQuery.of(context).size.width,
         child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              SizedBox(
-                height: MediaQuery.of(context).size.width * 0.7,
-              ),
-              SizedBox(
-                width: double.infinity,
-                child: Text(
-                  'Instagram',
-                  textAlign: TextAlign.center,
-                  style: GoogleFonts.lobsterTwo(
-                      fontSize: 30, fontWeight: FontWeight.bold),
+          child: ScaleTransition(
+            scale: _animation,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                SizedBox(
+                  height: MediaQuery.of(context).size.width * 0.7,
                 ),
-              ),
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                child: TextField(
-                  textInputAction: TextInputAction.done,
-                  onChanged: (value) {
-                    if (value.length > 6) {
-                      FocusScope.of(context).requestFocus(focusNode2);
-                    }
-                  },
-                  focusNode: focusNode1,
-                  controller: controller,
-                  decoration: const InputDecoration(
-                      isDense: true,
-                      hintText: 'Name',
-                      border: OutlineInputBorder()),
-                ),
-              ),
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                child: TextField(
-                  textInputAction: TextInputAction.next,
-                  decoration: InputDecoration(
-                      isDense: true,
-                      hintText: 'Email',
-                      border: OutlineInputBorder()),
-                ),
-              ),
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                child: TextField(
-                  textInputAction: TextInputAction.next,
-                  focusNode: focusNode2,
-                  decoration: const InputDecoration(
-                      isDense: true,
-                      hintText: 'Password',
-                      border: OutlineInputBorder()),
-                ),
-              ),
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                child: SizedBox(
+                SizedBox(
                   width: double.infinity,
-                  child: CupertinoButton.filled(
-                      borderRadius: BorderRadius.circular(3),
-                      child: const Text('Sign up'),
-                      onPressed: () {}),
+                  child: Text(
+                    key: const ValueKey<String?>(''),
+                    'Instagram',
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.lobsterTwo(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black),
+                  ),
                 ),
-              ),
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                child: Row(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text('Already have an account?'),
-                    TextButton(
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                        child: const Text('Login'))
-                  ],
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                  child: TextField(
+                    textInputAction: TextInputAction.done,
+                    onChanged: (value) {
+                      if (value.length > 6) {
+                        FocusScope.of(context).requestFocus(focusNode2);
+                      }
+                    },
+                    focusNode: focusNode1,
+                    controller: controller,
+                    decoration: const InputDecoration(
+                        isDense: true,
+                        hintText: 'Name',
+                        border: OutlineInputBorder()),
+                  ),
                 ),
-              ),
-            ],
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                  child: TextField(
+                    textInputAction: TextInputAction.next,
+                    decoration: InputDecoration(
+                        isDense: true,
+                        hintText: 'Email',
+                        border: OutlineInputBorder()),
+                  ),
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                  child: TextField(
+                    textInputAction: TextInputAction.next,
+                    focusNode: focusNode2,
+                    decoration: const InputDecoration(
+                        isDense: true,
+                        hintText: 'Password',
+                        border: OutlineInputBorder()),
+                  ),
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: CupertinoButton.filled(
+                        borderRadius: BorderRadius.circular(3),
+                        child: const Text('Sign up'),
+                        onPressed: () {}),
+                  ),
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text('Already have an account?'),
+                      TextButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: const Text('Login'))
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
